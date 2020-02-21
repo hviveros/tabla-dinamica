851d9d742113afe3888a62fabc51fad61b014029
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-02-20 22:19:23 -0300

    Maquetacion de tabla

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tablas dinamicas con Datatable y PHP</title>
+	<?php 
+		require_once "scripts.php";
+	?>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="card text-left">
+					<div class="card-header">
+						Tablas dinamicas con Datatable y PHP
+					</div>
+					<div class="card-body">
+						<button class="btn btn-primary">Agregar nuevo</button>
+						<hr>
+						<div id="tablaDatatable">
+							
+						</div>
+					</div>
+					<div class="card-footer text-muted">
+						@hviveros
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#tablaDatatable').load('tabla.php');
+	})
+</script>
---
+++ tabla.php
@@ -0,0 +1,23 @@
+<div>
+	<table class="table table-hover table-condensed" id="iddatatable">
+		<thead style="background-color: #dc3545; color: white; font-weight: bold;">
+			<tr>
+				<td>Nombre</td>
+				<td>Año</td>
+				<td>Empresa</td>
+			</tr>
+		</thead>
+		<tfoot style="background-color: #ccc; color: white; font-weight: bold;">
+			<tr>
+				<td>Nombre</td>
+				<td>Año</td>
+				<td>Empresa</td>
+			</tr>
+		</tfoot>
+		<tbody>
+			<tr></tr>
+			<tr></tr>
+			<tr></tr>
+		</tbody>
+	</table>
+</div>
